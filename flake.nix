e688dc086b72c062be3dcf1c4d37329087e13ac0
Luc Perkins <lucperkins@gmail.com>
2025-06-03 10:28:42 -0700

    Known to work

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Fixing hash mismatch demo";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ go ];
+        };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.buildGoModule {
+          name = "hash-mismatch-demo";
+          src = builtins.path { path = ./.; name = "hash-mismatch-demo-source"; };
+          vendorHash = "sha256-7c9WFEFaftTkPUmKa/SSCy/7djKbmcxM3Jt8RZv5un0=";
+        };
+      });
+    };
+}
